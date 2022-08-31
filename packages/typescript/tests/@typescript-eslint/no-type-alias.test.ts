c81bfb5a481f4eb2f073b29d47c715b8d009a5c8
David GABISON <david.gabison@outlook.com>
2022-08-31 09:40:42 +0200

    fix: @typescript-eslint/no-type-alias allow conditional types

--- packages/typescript/tests/@typescript-eslint/no-type-alias.test.ts
+++ packages/typescript/tests/@typescript-eslint/no-type-alias.test.ts
@@ -1,3 +1,4 @@
+export type Conditional<T> = T extends string ? T : never;
+export type Falsy = 0 | false | null | undefined;
 export type Generic = Exclude<PropertyKey, string>;
 export type Mapped<T> = { [K in keyof T]: T[K] };
-export type Falsy = 0 | false | null | undefined;
